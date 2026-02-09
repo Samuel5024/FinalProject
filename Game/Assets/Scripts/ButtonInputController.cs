898138dc81d7fb0b3d33580871a54881d26e4a95
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-02-08 18:43:05 -0700

    add ResetAfterDelay.cs to bullet prefab

--- Game/Assets/Scripts/ButtonInputController.cs
+++ Game/Assets/Scripts/ButtonInputController.cs
@@ -6,16 +6,16 @@ using UnityEngine.Events;
 public class ButtonInputController : MonoBehaviour
 {
     public UnityEvent ButtonDownEvent = new UnityEvent();
-    public UnityEvent ButtonUpEvent = new UnityEvent();
+    //public UnityEvent ButtonUpEvent = new UnityEvent();
     void Update()
     {
         if(Input.GetButtonDown("XRI_Right_TriggerButton"))
         {
             ButtonDownEvent.Invoke();
         }
-        else if(Input.GetButtonUp("XRI_Right_TriggerButton"))
-        {
-            ButtonUpEvent.Invoke();
-        }
+        // else if(Input.GetButtonUp("XRI_Right_TriggerButton"))
+        // {
+        //     ButtonUpEvent.Invoke();
+        // }
     }
 }

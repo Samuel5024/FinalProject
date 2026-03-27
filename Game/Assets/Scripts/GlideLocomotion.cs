5257ddc182e28a2e5cf5b08b0388952b8b487ddf
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-03-26 17:40:30 -0700

    Work on movement system.

--- Game/Assets/Scripts/GlideLocomotion.cs
+++ Game/Assets/Scripts/GlideLocomotion.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
 
-public class GlideLocomotion : MonoBehaviour
+public class GlideLocomotion : LocomotionProvider
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform rigRoot;
+    public float velocity = 10f; // meters per second
+    public float rotationSpeed = 100f; // degrees per second
+    public Transform trackedTransform; // camera or controller, null for thumbstick
+    private bool isMoving;
+    private void Start()
     {
-        
+        if(rigRoot == null) // initialize rigroot to game object's transform
+        {
+            rigRoot = transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(!isMoving && !CanBeginLocomotion())
+        {
+            return;
+        }
+        float forward = Input.GetAxis("XRI_Right_Primary2DAxis_Vertical");
+        float sideways = Input.GetAxis("XRI_Right_Primary2DAxis_Horizontal");
+
+        if(forward == 0f && sideways == 0f)
+        {
+            isMoving = false;
+            EndLocomotion();
+            return;
+        }
+
+        if(!isMoving)
+        {
+            isMoving = true;
+            BeginLocomotion();
+        }
+
+        if(forward != 0f)
+        {
+            Vector3 moveDirection = Vector3.forward;
+            if(trackedTransform != null)
+            {
+                moveDirection = trackedTransform.forward;
+                moveDirection.y = 0f;
+            }
+            moveDirection *= -forward * velocity * Time.deltaTime;
+            rigRoot.Translate(moveDirection);
+        }
+
+        if(trackedTransform == null && sideways != 0f)
+        { 
+            if(sideways != 0f)
+            {
+                float rotation = sideways * rotationSpeed * Time.deltaTime;
+                rigRoot.Rotate(0, rotation, 0);
+            }
+        }    
     }
 }

ddd17e07b1a6476902bae9bcb0a1850cc8f535f6
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-04-07 16:52:31 -0700

    Use the Move function to push the character forward.

--- Game/Assets/Scripts/PlayerBehavior.cs
+++ Game/Assets/Scripts/PlayerBehavior.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public CharacterController characterController;
     public float movementSpeed = 3.0f;
-    public float rotationSpeed = 90.0f;
     
 
-    // void Update()
-    // {
-    //     characterController.AddForce(0, 0, movementSpeed * Time.deltaTime);        
-    // }
+    void Update()
+    {
+        Vector3 Move = new Vector3(0, 0, movementSpeed * Time.deltaTime);
+        characterController.Move(Move);        
+    }
 }

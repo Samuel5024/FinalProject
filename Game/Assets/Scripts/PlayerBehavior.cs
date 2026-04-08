8f71adfea525fd374cdfa757bfc59a445634435d
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-04-07 17:06:51 -0700

    Implement a downwardForce into the Vector3 Move function
    When the player is using the control stick while falling off, the player shoots downward rapidly.

--- Game/Assets/Scripts/PlayerBehavior.cs
+++ Game/Assets/Scripts/PlayerBehavior.cs
@@ -4,11 +4,12 @@ public class PlayerBehavior : MonoBehaviour
 {
     public CharacterController characterController;
     public float movementSpeed = 3.0f;
+    public float downwardForce = -5.0f;
     
 
     void Update()
     {
-        Vector3 Move = new Vector3(0, 0, movementSpeed * Time.deltaTime);
+        Vector3 Move = new Vector3(0, downwardForce *Time.deltaTime, movementSpeed * Time.deltaTime);
         characterController.Move(Move);        
     }
 }

f8bd3f35f52d91dfb214ca3bbc245802de8ced16
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-04-09 21:29:24 -0700

    Fix PlayerJump.cs syntax errors

--- Game/Assets/Scripts/PlayerJump.cs
+++ Game/Assets/Scripts/PlayerJump.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.InputSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
@@ -12,5 +12,36 @@ public class PlayerJump : MonoBehaviour
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        jumpButton.action.performed += Jumping;
+    }
+
+
+    private void OnDisable()
+    {
+        jumpButton.action.performed -= Jumping;
+    }
+
+    private void Jumping(InputAction.CallbackContext obj)
+    {
+        if(!characterController.isGrounded)
+        {
+            return;
+        }
+
+        playerVelocity.y += Mathf.Sqrt(f:jumpHeight * -3.0f * gravityValue);
+    }
+
+    private void Update()
+    {
+        if(characterController.isGrounded && playerVelocity.y < 0) 
+        {
+            playerVelocity.y = 0f;
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
     }
 }
